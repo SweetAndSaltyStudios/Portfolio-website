c9bc57ca33dc8097a917dd0d4d09eb29aea71a83
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-10-20 18:04:05 +0300

    system-display tab bar created

--- system-display/js/tabs.js
+++ system-display/js/tabs.js
@@ -1,31 +1,31 @@
-
-function create_tabs(parent_element, content_data)
+function create_tab_elements(content_data_objects)
 {
-    for (i = 0; i < content_data.length; i++) 
+    const tabs_sidebar = document.querySelector('.tab__sidebar');
+    const tab_contents = document.querySelector('.tab_contents');
+    
+    id = 1;
+    
+    content_data_objects.forEach(data_object => 
     {
-        const new_tab_element = `<button class="tab__button" data-for-tab="${i + 1}">${content_data[i].title}</button>`;
-        
-        parent_element.innerHTML += new_tab_element;
-    };
+        let new_list_elements = [];
+
+        tabs_sidebar.innerHTML += 
+        `<button class="tab__button" data-for-tab="${id}">${data_object.title}</button>`;
+
+        for (const [key, value] of data_object.content_map.entries()) 
+        {
+            new_list_elements += `<h2>${key}</h2><li>${value}</li>`
+        }
+
+        tab_contents.innerHTML +=
+        `<div class="tab__content" data-for-tab="${id}">${new_list_elements}</div>`
+
+        id++;
+    });
+
 }
 
-function create_contents(parent_element, content_data)
-{
-    for (i = 0; i < content_data.length; i++) 
-    {
-        const new_content_element = 
-        `<div class="tab__content" data-for-tab="${i + 1}">
-        <h2>${content_data[i].title}</h2>
-        <p>
-            ${content_data[i].content}
-        </p>
-        </div>`
-        
-        parent_element.innerHTML += new_content_element;
-    }
-}
-
-function setupTabs(tab__buttons, tab__contents) 
+function setup_tabs(tab__buttons, tab__contents) 
 {
     tab__buttons.forEach(button => 
     {
@@ -51,21 +51,87 @@ function setupTabs(tab__buttons, tab__contents)
 
 document.addEventListener('DOMContentLoaded', () => 
 {
-    const tabs_sidebar = document.querySelector('.tab__sidebar');
-    const tab_contents = document.querySelector('.tab_contents');
+    const content_data_objects = [
+        { 'title' : 'GENERAL', 'content_map' : general_info_map }, 
+        { 'title' : 'BROWSER', 'content_map' : browser_info_map },
+        { 'title' : 'DEVICE', 'content_map' : device_info_map },
+        { 'title' : 'NETWORK', 'content_map' : newtwork_info_map },
+        { 'title' : 'MIME', 'content_map' : mime_info_map },
+        { 'title' : 'OTHER', 'content_map' : other_info_map}];
 
-    const content_data = [{ title:'Title 1', content : 'Content 1'}, { title:'Title 2', content : 'Content 2'}];
+    create_tab_elements(content_data_objects);
 
-    create_tabs(tabs_sidebar, content_data);
-    create_contents(tab_contents, content_data);
+    const tab__buttons = document.querySelectorAll('.tab__button');
+    const tab__content = document.querySelectorAll('.tab__content');
 
-    const tab__buttons =  document.querySelectorAll('.tab__button');
-    const tab__contents = document.querySelectorAll('.tab__content');
-
-    setupTabs(
-        tab__buttons,
-        tab__contents,
-    );
+    setup_tabs(tab__buttons, tab__content);
     
     tab__buttons[0].click();
 });
+
+const general_info_map =  new Map([
+    ['Online', navigator.onLine],
+    ['Languages', navigator.languages],
+    ['Language', navigator.language],
+
+    // Specific to Internet Explorer and Microsoft Edge.
+    // ['Ms Manipulation views enabled: ', navigator.msManipulationViewsEnabled],
+    // ['Ms Max touuch points: ' , navigator.msMaxTouchPoints],
+    // ['Ms Pointer enabled: ', navigator.msPointerEnabled],
+    
+    // In late 2013, 
+    // pointerEnabled was removed from the specification as checking PointerEvent in
+    // Window object is sufficient for feature detection. 
+    // Do not use this property and use PointerEvent instead.
+    // ['Pointer enabled: ' , navigator.pointerEnabled],
+]);
+
+const browser_info_map = new Map([
+    ['App name', navigator.appName],
+    ['App code name', navigator.appCodeName],
+    ['App version', navigator.appVersion],
+    ['Platform', navigator.platform],
+    ['Web driver', navigator.webdriver],
+    ['Is cookies enabled', navigator.cookieEnabled],
+    ['Do not track', navigator.doNotTrack],
+]);
+
+const device_info_map = new Map([
+    ['Operating system', navigator.oscpu],
+    ['Max touch points', navigator.maxTouchPoints],
+    ['Media devices', navigator.mediaDevices],
+    ['Device memory', `This device has at least ${navigator.deviceMemory}GiB of RAM.`],
+    ['Hardware concurrency', `${navigator.hardwareConcurrency} logical processors available to run threads on the device.}`]
+]);
+
+// const bar = ;
+// console.log(bar);
+
+const newtwork_info_map = new Map([
+    ["CONNECTON", navigator.connection],
+]);
+
+const mimeTypes = navigator.mimeTypes;
+const mime_info_map = new Map();
+const mimeLenth = mimeTypes.length;
+
+mime_info_map.set('Count', `${mimeLenth}`);
+
+for(var i = 0; i < mimeLenth; i++)
+{
+    mime_info_map.set(`Mime ${i + 1}`, mimeTypes[i]);
+};
+
+const other_info_map = new Map([
+    ['Clipboard', navigator.clipboard],
+    ['Credentials', navigator.credentials],
+    ['Geolocation', navigator.geolocation],
+    ['Permissions', navigator.permissions],
+    ['Product', navigator.product],
+    ['Product sub', navigator.productSub],
+    ['Service worker', navigator.serviceWorker],
+    ['Storage', navigator.storage],
+    ['Vendor', navigator.vendor],
+    ['Vendor sub', navigator.vendorSub],
+    ['User agent', navigator.userAgent],
+]);
